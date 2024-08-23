091e440407d15b75a0a848e693f75659ce4ec332
Roberto Andrade <120339648+Roberto10Andrade@users.noreply.github.com>
2024-08-22 22:04:52 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,40 +2,55 @@ const express = require('express')
 const app = express()
 const port = 3001
 const path = require('path')
+const session = require('express-session');
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser');
+
+
 
 //Anexa a pasta templates
-const basePath = path.join(__dirname, 'templates')
+//const basePath = path.join(__dirname, 'templates')
 
-//Parser de conversao de texto
+
+//Parser para leitura do body
 app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
   
 
-//index
-app.get('/', (req, res) => {
-    res.sendFile(`${basePath}/index.html`)
-  })
+
+//Config Session
+app.use(session({
+  secret: '098094809dssfsf584584338049385',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 60000 } // session timeout of 60 seconds
+}));
 
 
-  //novo usuario
-app.get('/users/add', (req, res) => {
-    res.sendFile(`${basePath}/newUser.html`)
-  })
+
+// Configurando o Handlebars como motor de visualização
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
 
 
-//salvar dados
-app.post('/users/save', (req, res) => {
-  adicionarUsuario(req.body.name, req.body.age)
-  res.sendFile(`${basePath}/newUser.html`);
+app.get('/dashboard', checkAuth, (req, res) => {
+  res.render(`dashboard`)
 })
 
 
+  
+app.get('/users/add', checkAuth, (req, res) => {
+  res.render(`userform`)
+})
 
-app.listen(port, () => {
-    console.log(`App rodando na porta ${port}`)
-  })
 
-
+//salvar dados
+app.post('/users/save', checkAuth, (req, res) => {
+  const name = req.body.name
+  const age = req.body.age
+  adicionarUsuario(name, age);
+  res.render(`dashboard`)
+})
 
   async function adicionarUsuario(name, age) {
     const url = 'http://localhost:3000/posts';
@@ -61,4 +76,81 @@ app.listen(port, () => {
     }
   }
 
+  async function deletarUsuario(id) {
+    const url = `http://localhost:3000/posts/${id}`; // Inclui o ID do usuário na URL
+  
+    try {
+      const response = await fetch(url, {
+        method: 'DELETE', // Define o método HTTP como DELETE
+        headers: {
+          'Content-Type': 'application/json' // Define o tipo de conteúdo como JSON
+        }
+      });
+  
+      if (response.ok) {
+        console.log('Usuário deletado com sucesso'); // Exibe uma mensagem de sucesso no console
+      } else {
+        console.error('Erro ao deletar usuário:', response.statusText); // Exibe uma mensagem de erro no console
+      }
+    } catch (error) {
+      console.error('Erro na requisição:', error); // Exibe uma mensagem de erro no console em caso de exceção
+    }
+  }
+  
+  app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    console.log(username, password);
+    // Authenticate user
+    if (username === 'admin' && password === 'admin') {
+      req.session.isLoggedIn = true;
+      req.session.username = username;
+  
+      res.render(`dashboard`)
+    } else {
+      res.render(`login`)
+    }
+  
+  
+  })
+  
+  function checkAuth(req, res, next) {
+    if (req.session.username) {
+      next();
+    } else {
+      res.render(`login`)
+    }
+  }
+  
+  
+  
+  app.get('/logout', (req, res) => {
+    req.session.destroy((err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Session destroyed');
+        res.render(`login`)
+      }
+    });
+  });
+  
+  
+  
+  
+  
+  app.get('/', (req, res) => {
+    const sessionData = req.session;
+    res.render(`login`)
+  })
+  
+  
+  
+
+  
+
+  
+  //webserver
+  app.listen(port, () => {
+    console.log('Server Started')
+  })
   
